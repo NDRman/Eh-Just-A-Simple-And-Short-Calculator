2058a0b099ab4d4bd425f2a441e481094df2ad73
NDRman <parthmahendra@outlook.com>
2017-07-17 20:29:41 +0100

    Adding More Functionality and Stopped Using Console

--- Calculator.py
+++ Calculator.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout, QLabel
 
 class App(QWidget):
 
@@ -8,26 +8,39 @@ class App(QWidget):
         super(App, self).__init__()
         global calculation
         calculation = ""
+        L1 = QLabel("")
+        L2 = QLabel("")
         self.layout = QGridLayout(self)
         t = 0
-        nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "/", "*", "(", ")"]
-        for row in range(4):
+        nums = ["0", "1", "2", "+", "3", "4", "5", "-", "6", "7", "8", "*", "9", "(", ")", "/", ".", "<-", "",
+         ""]
+        for row in range(2, 7):
             for col in range(4):
                 button = QPushButton(nums[t])
                 t += 1
-                button.clicked.connect(self.buttonClicked)
+                button.clicked.connect(lambda: self.buttonClicked(L1, L2))
                 self.layout.addWidget(button, row, col)
+
+
+        self.layout.addWidget(L1)
+        self.layout.addWidget(L2)
         self.setWindowTitle("Cowlculator")
+
         self.show()
 
-    def buttonClicked(self):
+    def buttonClicked(self, L1, L2):
         os.system('cls')
         global calculation
-        calculation += self.sender().text()
+        if self.sender().text() != "<-":
+            calculation += self.sender().text()
+        else:
+            calculation = calculation[:-1]
         try:
-            print(calculation, "\n\n", "=" , eval(calculation))
+            L1.setText(calculation)
+            L2.setText(str(eval(calculation)))
         except:
-            print(calculation)
+            L1.setText(calculation)
+            L2.setText("Err")
 
 app = QApplication(sys.argv)
 ex = App()
